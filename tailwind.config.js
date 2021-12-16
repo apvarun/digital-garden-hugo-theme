68e84c68dc5d1b88a6d0b63555b1b5ed6e09adc1
Varun A P <varunlakshmananap@gmail.com>
2021-12-16 23:28:27 +0100

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./content/**/*.md",
+    "./content/**/*.html",
+    "./layouts/**/*.html",
+    "./themes/digitalgarden/content/**/*.md",
+    "./themes/digitalgarden/content/**/*.html",
+    "./themes/digitalgarden/layouts/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
